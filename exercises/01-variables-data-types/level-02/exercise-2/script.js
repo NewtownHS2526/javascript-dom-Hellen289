670655fa60371bc529e15f9bc9a7d134ca56da1a
Hellen Arita <hellena3@nycstudents.net>
2025-11-18 14:41:12 -0500

    Exercise2-level2-Finished

--- exercises/01-variables-data-types/level-02/exercise-2/script.js
+++ exercises/01-variables-data-types/level-02/exercise-2/script.js
@@ -1,3 +1,6 @@
 // TODO: Create variables with good names: firstName, lastName, birthYear
 // TODO: Print them
-
+let firstName = "Hellen";
+let lastName = "Grace";
+let birthYear = 2013;
+console.log(firstName,lastName,birthYear);

1921add05954e15e4ea9f830d6be9a666bb0f045
Hellen Arita <hellena3@nycstudents.net>
2025-11-18 14:38:30 -0500

    Exercise1-level2-Finished

--- exercises/01-variables-data-types/level-02/exercise-1/script.js
+++ exercises/01-variables-data-types/level-02/exercise-1/script.js
@@ -1,3 +1,7 @@
 // TODO: Create variables for string, number, boolean, and undefined
 // TODO: Print each one
-
+let text = "Hi";
+let num = 59;
+let bool = false;
+let undef = undefined;
+console.log(text,num,bool,undef);

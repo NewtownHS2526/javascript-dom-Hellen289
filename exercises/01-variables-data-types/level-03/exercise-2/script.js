2ca342930a219c4dc5294df7c187b6a1fd4704c4
Hellen Arita <hellena3@nycstudents.net>
2025-11-19 13:50:16 -0500

    Finished-level3

--- exercises/01-variables-data-types/level-03/exercise-2/script.js
+++ exercises/01-variables-data-types/level-03/exercise-2/script.js
@@ -1,3 +1,6 @@
 // TODO: Create variables for age (number), city (string), and isStudent (boolean)
 // TODO: Print all three using console.log()
-
+let age=30;
+let city="New York";
+let isStudent=false;
+console.log(age,city,isStudent);
--- exercises/01-variables-data-types/level-03/exercise-3/script.js
+++ exercises/01-variables-data-types/level-03/exercise-3/script.js
@@ -1,4 +1,6 @@
 // TODO: Create const variable 'studentID' with a string
 // TODO: Create let variable 'grade' with a number
 // TODO: Print both
-
+const studentID="Stud3456";
+let grade=89;
+console.log(studentID,grade);

f906e47cab7a49db30e239cbd74ecd43697da4e0
Hellen Arita <hellena3@nycstudents.net>
2025-11-20 13:46:50 -0500

    03-level2-Finished

--- exercises/03-reassignment/level-02/exercise-1/script.js
+++ exercises/03-reassignment/level-02/exercise-1/script.js
@@ -1,3 +1,6 @@
 // TODO: Create const variable and try to reassign (will cause error)
 // TODO: Then create let variable and reassign successfully
-
+const fixed = "Cannot change";
+let changeable = "Can change";
+changeable = "New value";
+console.log(changeable);
